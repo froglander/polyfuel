8e9eadbe29a57270bdfb7067765fac1728fbd615
Kristi Heredia <froglander@dukworld.net>
2017-01-12 20:40:49 -0500

    Remove sign-up button for now

--- app/components/Header.js
+++ app/components/Header.js
@@ -53,11 +53,11 @@ export default class Header extends React.Component {
                                 </li>
                             </NotAuthenticated>
 
-                            <NotAuthenticated>
-                                <li>
-                                    <Link to="/SignUp">Sign Up</Link>
-                                </li>
-                            </NotAuthenticated>
+                            {/*<NotAuthenticated>*/}
+                                {/*<li>*/}
+                                    {/*<Link to="/SignUp">Sign Up</Link>*/}
+                                {/*</li>*/}
+                            {/*</NotAuthenticated>*/}
                             <Authenticated>
                                 <li>
                                     <LogoutLink/>
